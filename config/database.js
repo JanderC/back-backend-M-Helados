e8df995e88e0c4235c6655fbfebd6583cea8df27
JanderC <janderalexisc@gmail.com>
2026-04-26 00:14:24 -0400

    cambios

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ require('dotenv').config();
  * Configuración de la conexión a PostgreSQL
  */
 const pool = new Pool({
-  host: process.env.DB_HOST || '200.40.68.122',
+  host: process.env.DB_HOST || 'aws-1-us-west-2.pooler.supabase.com',
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'respaldo2',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'p4ng34t3ch',
+  database: process.env.DB_NAME || 'postgres',
+  user: process.env.DB_USER || 'postgres.xorvhmunbqfhvwftpjcb',
+  password: process.env.DB_PASSWORD || 'Mhelados123*',
   max: 20, // Máximo de conexiones en el pool
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
